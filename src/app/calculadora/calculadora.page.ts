45a287b3c7689f1539cef4605a596b7bbffddd48
MMAGES2006 <22170@portalcedi.edu.mx>
2025-03-07 20:01:39 -0600

    Progreso de la tabla de datos

--- src/app/calculadora/calculadora.page.ts
+++ src/app/calculadora/calculadora.page.ts
@@ -10,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculadoraPage implements OnInit {
 
+  items: {
+    id: number; ubiA: string; ubiB: string, product: string, quan: string, 
+  }[] = [];
+  nextId: number = 1;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+
+  addItem(ubiA: string, ubiB: string, product: string, quan: string): void 
+  {
+    if (ubiA.trim() && ubiB.trim() && product.trim() && quan.trim()) {
+      this.items.push({
+        id: this.nextId++,
+        ubiA: ubiA.trim(),
+        ubiB: ubiB.trim(),
+        product: product.trim(),
+        quan: quan.trim(),
+      });
+    } else {
+      console.error('Todos los espacios deben ser llenados');
+    }
+  }
 }
